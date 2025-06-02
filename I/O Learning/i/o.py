f10fa6767f11046b4355474839709583c9788352
SinghDewang07 <singh.dewang007@gmail.com>
2025-06-02 10:34:10 +0000

    I/O pdate

--- I/O Learning/i/o.py
+++ I/O Learning/i/o.py
@@ -1,5 +1,43 @@
-f = open("/workspaces/LearningPython/I/O Learning/hello.txt", "a")
+# f = open("/workspaces/LearningPython/I/O Learning/hello.txt", "a")
 
-data = f.write("i m learning python today")
+# data = f.write("i m learning python today")
 
-f.close()
+# f.close()
+
+
+# now reading the content for files can be done by two methods.
+
+# 1.  data = f.read()
+# 2.  dta  = f.readline()
+
+
+# f = open("/workspaces/LearningPython/I/O Learning/hello.txt" , "r")
+# data = f.read()
+
+# print(data)
+# f.close()
+
+
+
+# f = open("/workspaces/LearningPython/I/O Learning/hello.txt" , "r")
+# data = f.readline()
+# print(data, end = "")
+
+# data1 = f.readline()
+# print(data1, end = "")
+
+# data2 = f.readline()
+# print(data2)
+
+
+# f.close()
+
+
+# f= open("/workspaces/LearningPython/I/O Learning/hello.txt" , "a")
+# data = f.write("Hello dear yes i m sure you learn python and DSA")
+
+# f.close()
+
+
+
+
